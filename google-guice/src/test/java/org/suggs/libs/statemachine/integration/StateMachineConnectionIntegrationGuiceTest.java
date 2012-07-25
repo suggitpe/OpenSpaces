b82591d0b0f6459120b1711920b67acf0379af20
Peter Suggitt <me@suggs.org.uk>
2012-07-25 09:00:43 +0100

    removed crap

---
+++ google-guice/src/test/java/org/suggs/libs/statemachine/integration/StateMachineConnectionIntegrationGuiceTest.java
@@ -0,0 +1,11 @@
+package org.suggs.libs.statemachine.integration;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: suggitpe
+ * Date: 25/07/12
+ * Time: 08:54
+ * To change this template use File | Settings | File Templates.
+ */
+public class StateMachineConnectionIntegrationGuiceTest {
+}
